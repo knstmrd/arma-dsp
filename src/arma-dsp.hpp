498ee863639d4fa1683f1e83cfaee7b42cde8047
George Oblapenko <kunstmord@kunstmord.com>
2017-08-01 16:50:35 +0300

    header guards

--- src/arma-dsp.hpp
+++ src/arma-dsp.hpp
@@ -1,3 +1,6 @@
+#ifndef arma_dsp_hpp
+#define arma_dsp_hpp
+
 #include <armadillo>
 #include <math>
 #include <function>
@@ -16,3 +19,5 @@ cx_mat stft(arma::vec signal, arma::vec window_array, int framelength=1024, int 
 cx_mat stft(arma::vec signal, std::function<arma::vec(int)> window, int framelength=1024, int overlap=2, bool half=true);
 
 cx_mat stft(arma::vec signal, std::function<arma::vec(int, double)> window, int framelength=1024, int overlap=2, bool half=true);
+
+#endif
--- src/windows.hpp
+++ src/windows.hpp
@@ -1,3 +1,6 @@
+#ifndef arma_dsp_windows_hpp
+#define arma_dsp_windows_hpp
+
 #include <armadillo>
 #include <math>
 
@@ -6,3 +9,5 @@ arma::vec window_gaussian(int framelength, double sigma);
 arma::vec window_hann(int framelength);
 arma::vec window_hamming(int framelength);
 arma::vec window_blackman(int framelength, double alpha);
+
+#endif
