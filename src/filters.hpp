fa1a01ff6f7ae4e1fd752385c55b85e4dceb8273
George Oblapenko <kunstmord@kunstmord.com>
2017-08-07 17:13:23 +0300

    some particular cases of FIR and IIR filters added

--- src/filters.hpp
+++ src/filters.hpp
@@ -8,28 +8,18 @@
 
 void fir_filter(const arma::vec &signal, const arma::vec &ff_coeffs, arma::vec &output);
 
-void fir_filter(const arma::vec &signal, double ff_coeff, int delay, arma::vec &output);
-
 void fir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int delay, arma::vec &output);
 
 arma::vec fir_filter(const arma::vec &signal, const arma::vec &ff_coeffs);
-
-arma::vec fir_filter(const arma::vec &signal, double ff_coeff, int delay);
-// y[n] = ff_coeff * x[n - delay]
 
 arma::vec fir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int delay);
 // y[n] = input_gain * x[n] + ff_coeff * x[n - delay]
 
 void iir_filter(const arma::vec &signal, const arma::vec &ff_coeffs, const arma::vec &fb_coeffs, arma::vec &output);
 
-void iir_filter(const arma::vec &signal, double ff_coeff, int ff_delay, double fb_coeff, int fb_delay, arma::vec &output);
-
 void iir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int ff_delay, double fb_coeff, int fb_delay, arma::vec &output);
 
 arma::vec iir_filter(const arma::vec &signal, const arma::vec &ff_coeffs, const arma::vec &fb_coeffs);
-
-arma::vec iir_filter(const arma::vec &signal, double ff_coeff, int ff_delay, double fb_coeff, int fb_delay);
-// y[n] = ff_coeff * x[n - ff_delay] - fb_coeff * y[n - fb_delay]
 
 arma::vec iir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int ff_delay, double fb_coeff, int fb_delay);
 // y[n] = input_gain * x[n] + ff_coeff * x[n - ff_delay] - fb_coeff * y[n - fb_delay]
