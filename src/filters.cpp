889dcadcba24fd1e4e4c819508b17bffee334e45
George Oblapenko <kunstmord@kunstmord.com>
2017-08-08 17:33:59 +0300

    fixed some bugs, added FIR and IIR filters for a single delay time

--- src/filters.cpp
+++ src/filters.cpp
@@ -9,7 +9,25 @@ void fir_filter(const arma::vec &signal, const arma::vec &ff_coeffs, arma::vec &
     for (i=ff_coeffs.n_elem; i<signal.n_elem; i++) {
         output[i] = arma::dot(signal.subvec(i, i - signal.n_elem + 1), ff_coeffs); // TODO: Does reversing indices work?
     }
+}
 
+void fir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int delay, arma::vec &output) {
+    int i;
+    output.subvec(0, delay-1) = input_gain * signal.subvec(0, delay-1);
+    output.subvec(delay, signal.n_elem-1) = input_gain * signal.subvec(delay, signal.n_elem-1) + ff_coeff * signal.subvec(0, signal.n_elem - delay - 1);
+}
+
+arma::vec fir_filter(const arma::vec &signal, const arma::vec &ff_coeffs) {
+    arma::vec output(signal.n_elem);
+
+    fir_filter(signal, ff_coeffs, output);
+    return output;
+}
+
+arma::vec fir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int delay) {
+    arma::vec output(signal.n_elem);
+
+    fir_filter(signal, input_gain, ff_coeff, delay, output);
     return output;
 }
 
@@ -25,21 +43,28 @@ void iir_filter(const arma::vec &signal, const arma::vec &ff_coeffs, const arma:
     for (i=fb_coeffs.n_elem+1; i<=signal.n_elem; i++) {
         output[i] = arma::dot(signal.subvec(i-1, i - signal.n_elem + 1), fb_coeffs);
     }
-
-    return output;
 }
 
-arma::vec fir_filter(const arma::vec &signal, const arma::vec &ff_coeffs, arma::vec &output) {
-    arma::vec output(signal.n_elem);
+void iir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int ff_delay, double fb_coeff, int fb_delay, arma::vec &output) {
+    fir_filter(signal, input_gain, ff_coeff, ff_delay, output);
 
-    fir_filter(signal, ff_coeffs, output);
-    return output;
+    for (i=fb_delay, i<signal.n_elem; i++) {
+        output[i] -= fb_coeff * output[i - fb_delay];
+    }
 }
+// y[n] = input_gain * x[n] + ff_coeff * x[n - ff_delay] - fb_coeff * y[n - fb_delay]
 
 arma::vec iir_filter(const arma::vec &signal, const arma::vec &ff_coeffs, const arma::vec &fb_coeffs) {
     arma::vec output(signal.n_elem);
 
     iir_filter(signal, ff_coeffs, fb_coeffs, output);
+    return output;
+}
+
+arma::vec iir_filter(const arma::vec &signal, double input_gain, double ff_coeff, int ff_delay, double fb_coeff, int fb_delay) {
+    arma::vec output(signal.n_elem);
+
+    iir_filter(signal, input_gain, ff_coeff, ff_delay, fb_coeff, fb_delay, output);
     return output;
 }
 
