317d9ca83ac1298eaea8ff57a9bfef5c93f2fbe2
George Oblapenko <kunstmord@kunstmord.com>
2017-08-10 18:10:09 +0300

    moved STFT functions to separate file

--- src/arma-dsp.hpp
+++ src/arma-dsp.hpp
@@ -1,16 +1,7 @@
 #ifndef arma_dsp_hpp
 #define arma_dsp_hpp
 
-#include <armadillo>
-#include <cmath>
-#include <function>
 #include "windows.hpp"
 #include "filters.hpp"
-
-cx_mat stft(arma::vec signal, arma::vec window_array, int framelength=1024, int overlap=2, bool half=true);
-
-cx_mat stft(arma::vec signal, std::function<arma::vec(int)> window, int framelength=1024, int overlap=2, bool half=true);
-
-cx_mat stft(arma::vec signal, std::function<arma::vec(int, double)> window, int framelength=1024, int overlap=2, bool half=true);
 
 #endif
