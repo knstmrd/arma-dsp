37fb5dafe32ccf89d981f286dba840615b7dd3d1
George Oblapenko <kunstmord@kunstmord.com>
2017-08-12 13:51:08 +0300

    removed redundant function declaration

--- src/stft.hpp
+++ src/stft.hpp
@@ -9,6 +9,4 @@ cx_mat stft(arma::vec signal, arma::vec window_array, int framelength=1024, int 
 
 cx_mat stft(arma::vec signal, std::function<arma::vec(int)> window, int framelength=1024, int overlap=2, bool half=true);
 
-cx_mat stft(arma::vec signal, std::function<arma::vec(int, double)> window, int framelength=1024, int overlap=2, bool half=true);
-
 #endif
