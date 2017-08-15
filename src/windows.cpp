bd6e09e92bff6662ecd6f619db15a9e74e495bf1
George Oblapenko <kunstmord@kunstmord.com>
2017-08-15 18:26:08 +0300

    fixed Blackman window

--- src/windows.cpp
+++ src/windows.cpp
@@ -63,6 +63,6 @@ arma::vec window_blackman(unsigned int framelength, double alpha) {
     }
     else {
         arma::vec n = arma::linspace<arma::vec>(0, framelength-1, framelength);
-        return (1.0 - alpha / 2.0) - 0.5 * arma::cos(2 * n * M_PI / (framelength - 1)) + 0.5 * alpha * arma::cos(4 * n * M_PI / (framelength - 1));
+        return (1.0 - alpha) / 2.0 - 0.5 * arma::cos(2 * n * M_PI / (framelength - 1)) + 0.5 * alpha * arma::cos(4 * n * M_PI / (framelength - 1));
     }
 }
