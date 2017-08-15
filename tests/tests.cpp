9ef63b264f5970a826f7c8353ad934a55ab449a5
George Oblapenko <kunstmord@kunstmord.com>
2017-08-15 18:25:43 +0300

    tests for remaining window functions and arma::vec-based FIR filter added

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -38,14 +38,18 @@ TEST_CASE( "Gaussian window function work correctly", "[window]" ) {
     REQUIRE( a.n_elem == 3 );
     REQUIRE( b.n_elem == 4 );
 
-    // REQUIRE( fabs(a[0] - 0.5) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[2] - 0.5) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[0] - 0.13533528) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[2] - 0.13533528) < MAX_ABSOLUTE_ERROR );
 
-    // REQUIRE( fabs(b[0] - 0.25) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[1] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[2] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[3] - 0.25) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[0] - 0.011109) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[1] - 0.60653066) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[2] - 0.60653066) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[3] - 0.011109) < MAX_ABSOLUTE_ERROR );
+
+    REQUIRE( fabs(c[0] - 0.36044779) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(c[1] - 1.) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(c[2] - 0.36044779) < MAX_ABSOLUTE_ERROR );
 }
 
 TEST_CASE( "Hann window function work correctly", "[window]" ) {
@@ -55,14 +59,14 @@ TEST_CASE( "Hann window function work correctly", "[window]" ) {
     REQUIRE( a.n_elem == 3 );
     REQUIRE( b.n_elem == 4 );
 
-    // REQUIRE( fabs(a[0] - 0.5) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[2] - 0.5) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[0] - 0.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[2] - 0.0) < MAX_ABSOLUTE_ERROR );
 
-    // REQUIRE( fabs(b[0] - 0.25) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[1] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[2] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[3] - 0.25) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[0] - 0.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[1] - 0.75) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[2] - 0.75) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[3] - 0.0) < MAX_ABSOLUTE_ERROR );
 }
 
 TEST_CASE( "Hamming window function work correctly", "[window]" ) {
@@ -72,30 +76,55 @@ TEST_CASE( "Hamming window function work correctly", "[window]" ) {
     REQUIRE( a.n_elem == 3 );
     REQUIRE( b.n_elem == 4 );
 
-    // REQUIRE( fabs(a[0] - 0.5) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[2] - 0.5) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[0] - 0.08) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[2] - 0.08) < MAX_ABSOLUTE_ERROR );
 
-    // REQUIRE( fabs(b[0] - 0.25) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[1] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[2] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[3] - 0.25) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[0] - 0.08) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[1] - 0.77) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[2] - 0.77) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[3] - 0.08) < MAX_ABSOLUTE_ERROR );
 }
 
 TEST_CASE( "Blackman window function work correctly", "[window]" ) {
     arma::vec a = window_blackman(3);
     arma::vec b = window_blackman(4);
-    arma::vec c = window_blackman(3, 0.2);
 
     REQUIRE( a.n_elem == 3 );
     REQUIRE( b.n_elem == 4 );
 
-    // REQUIRE( fabs(a[0] - 0.5) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(a[2] - 0.5) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[0] - 0.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[1] - 1.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(a[2] - 0.0) < MAX_ABSOLUTE_ERROR );
 
-    // REQUIRE( fabs(b[0] - 0.25) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[1] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[2] - 0.75) < MAX_ABSOLUTE_ERROR );
-    // REQUIRE( fabs(b[3] - 0.25) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[0] - 0.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[1] - 0.63) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[2] - 0.63) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(b[3] - 0.0) < MAX_ABSOLUTE_ERROR );
+}
+
+TEST_CASE( "FIR filter working", "[filter][fir]" ) {
+    arma::vec signal = {2., 1., 0., 0.};
+    arma::vec coeffs_1 = {1.}; // this leaves the signal unchanged
+    arma::vec coeffs_2 = {1., 0.5}; 
+    arma::vec coeffs_3 = {1., 0.5, 0.25}; 
+
+    arma::vec output_1 = fir_filter(signal, coeffs_1);
+    REQUIRE( fabs(output_1[0] - 2.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_1[1] - 1.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_1[2] - 0.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_1[3] - 0.0) < MAX_ABSOLUTE_ERROR );
+
+    arma::vec output_2 = fir_filter(signal, coeffs_2);
+    REQUIRE( fabs(output_2[0] - 2.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_2[1] - 2.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_2[2] - 0.5) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_2[3] - 0.0) < MAX_ABSOLUTE_ERROR );
+
+    arma::vec output_3 = fir_filter(signal, coeffs_3);
+    REQUIRE( fabs(output_3[0] - 2.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_3[1] - 2.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_3[2] - 1.0) < MAX_ABSOLUTE_ERROR );
+    REQUIRE( fabs(output_3[3] - 0.25) < MAX_ABSOLUTE_ERROR );
+
 }
