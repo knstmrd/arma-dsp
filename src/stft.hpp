d016c1220ea2c8edf63468ec0f3ddcef40f0b4d5
George Oblapenko <kunstmord@kunstmord.com>
2017-08-21 16:00:07 +0300

    moved cmath include

--- src/stft.hpp
+++ src/stft.hpp
@@ -2,7 +2,6 @@
 #define arma_dsp_stft
 
 #include <armadillo>
-#include <cmath>
 #include <function>
 
 cx_mat stft(arma::vec signal, arma::vec window_array, int framelength=1024, int overlap=2, bool half=true);
