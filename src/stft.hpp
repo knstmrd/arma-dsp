8c0f8de2766f0f83d917ba4b7b39a7f00d121b17
George Oblapenko <kunstmord@kunstmord.com>
2017-08-21 16:48:43 +0300

    fixed std::functions include, added centered parameter and another overload of stft

--- src/stft.hpp
+++ src/stft.hpp
@@ -1,11 +1,13 @@
-##ifndef arma_dsp_stft
+#ifndef arma_dsp_stft
 #define arma_dsp_stft
 
 #include <armadillo>
-#include <function>
+#include <functional>
 
-cx_mat stft(arma::vec signal, arma::vec window_array, int framelength=1024, int overlap=2, bool half=true);
+arma::cx_mat stft(const arma::vec &signal, const arma::vec &window_array, int framelength=1024, int overlap=2, bool centered=true, bool half=true);
 
-cx_mat stft(arma::vec signal, std::function<arma::vec(int)> window, int framelength=1024, int overlap=2, bool half=true);
+arma::cx_mat stft(const arma::vec &signal, std::function<arma::vec(int)> window, int framelength=1024, int overlap=2, bool centered=true, bool half=true);
+
+arma::cx_mat stft(const arma::vec &signal, int framelength=1024, int overlap=2, bool centered=true, bool half=true);
 
 #endif
