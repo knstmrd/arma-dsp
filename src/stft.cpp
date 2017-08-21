894a6b72a2eea6364fdfd9be5f1593ac0c43f3e1
George Oblapenko <kunstmord@kunstmord.com>
2017-08-21 16:00:35 +0300

    base commit

---
+++ src/stft.cpp
@@ -0,0 +1,10 @@
+#include "stft.hpp"
+#include <cmath>
+
+cx_mat stft(arma::vec signal, arma::vec window_array, int framelength=1024, int overlap=2, bool half=true) {
+
+}
+
+cx_mat stft(arma::vec signal, std::function<arma::vec(int)> window, int framelength=1024, int overlap=2, bool half=true) {
+    
+}
